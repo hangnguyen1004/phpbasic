afcee9426e3f1a3daa41abc4696b41e0afaa142b
thangnd <thangnd@deha-soft.com>
2019-10-15 15:31:27 +0700

    update bai 3

--- bai3_thangnd.php
+++ bai3_thangnd.php
@@ -10,27 +10,30 @@
     $c='';
     $x1='';
     $x2='';
+    print "nhap a,b,c: ";
+    fscanf (STDIN,"%d\n", $a, $b, $c);
     if($a==0)
     {
         echo "x1=x2=" .$x1=(-$b/$c);
     }
     else if($a==0 & $b==0 & $c==0)
     {
-
+        echo "phuong trinh vo so nghiem ";
     }
-    $delta=($b*$b)-(4*$a*$c);
-    if($delta < 0)
+    else if($a==0 & $b==0)
     {
-        echo "pt vo nghiem";
+        echo "phuong trinh vo nghiem";
     }
-    else if($delta == 0)
-    {
-        $x1=$x2=(-$b/2*$a);
-        echo "pt co nghiem kep x1=x2=" .$x1;
-    }
-    else
-    {
-        echo "pt co 2 nghiem x1=" .$x1=((-$b + sqrt($delta))/2*$a);
-        echo "x2=" .$x2=((-$b - sqrt($delta))/2*$a);
+    else {
+        $delta = ($b * $b) - (4 * $a * $c);
+        if ($delta < 0) {
+            echo "pt vo nghiem";
+        } else if ($delta == 0) {
+            $x1 = $x2 = (-$b / 2 * $a);
+            echo "pt co nghiem kep x1=x2=" . $x1;
+        } else {
+            echo "pt co 2 nghiem x1=" . $x1 = ((-$b + sqrt($delta)) / 2 * $a);
+            echo "x2=" . $x2 = ((-$b - sqrt($delta)) / 2 * $a);
+        }
     }
 ?>
